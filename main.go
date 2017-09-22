bba5b210c9fed58493481fdcac0c6a85cd3a68fe
Mario Zupan <mzupanmz@gmail.com>
2017-09-22 10:25:08 +0200

    init main.go

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-chi/cors"
+	"github.com/pressly/chi"
+	"log"
+	"net/http"
+)
+
+type result struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
+	Data    string `json:"data"`
+}
+
+func (s *result) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
 
 func main() {
 	fmt.Println("tada!")
+	r := chi.NewRouter()
+	corsOption := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+	r.Use(corsOption.Handler)
+	r.Use(middleware.Logger)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+	r.Get("")
+
+	log.Info("Server started on localhost:8080")
+	log.Fatal(http.ListenAndServeTLS(":8080", r))
+}
+
+func createAgreementHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		render.Render(w, r, &result{Success: true})
+	})
 }
